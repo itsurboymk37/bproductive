a737d5d423ef9bb05765d7bf3692678b3290be86
itsurboymk37 <markush@nuevaschool.org>
2020-08-20 12:29:24 -0700

    Tagging is working in browser: show only

---
+++ app/controllers/tags_controller.rb
@@ -0,0 +1,9 @@
+class TagsController < ApplicationController
+    def show
+        @tag = Tag.find(params[:id])
+    end      
+end
+#what I want to do:
+#homepage should be a list of the tags, which you can then click on and expand to see the activities (and click again to hide them)
+    #eventually, want it to show like the top four for that tag, and then when you click on the tag it takes you to a list of all articles
+#center tags under the title (when you click on an individual article)
