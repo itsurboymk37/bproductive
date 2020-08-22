4639fefd0a11916f2bab86bd7f63afce2c2ed131
markush <markush@nuevaschool.org>
2020-08-22 11:41:16 -0700

    logging in/out

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
-  root to: 'tags#index'
+  root to: 'author_sessions#new'
   resources :activities
+
   resources :posts
+
   resources :tags
+
   resources :authors
+
+  resources :author_sessions, only: [ :new, :create, :destroy ]
+
+  get 'login'  => 'author_sessions#new'
+  get 'logout' => 'author_sessions#destroy'
 end
