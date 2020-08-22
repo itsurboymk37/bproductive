dc3bca37125b367d11fcf28fced5282b99b92aa6
markush <markush@nuevaschool.org>
2020-08-22 12:29:39 -0700

    added link to authors#index on home page (tags#index + show)

--- app/controllers/author_sessions_controller.rb
+++ app/controllers/author_sessions_controller.rb
@@ -4,7 +4,7 @@ class AuthorSessionsController < ApplicationController
   
     def create
       if login(params[:email], params[:password])
-        redirect_back_or_to(activities_path, notice: 'Logged in successfully.')
+        redirect_back_or_to(tags_path, notice: 'Logged in successfully.')
       else
         flash.now.alert = "Login failed."
         render action: :new
@@ -13,6 +13,6 @@ class AuthorSessionsController < ApplicationController
   
     def destroy
       logout
-      redirect_to(:authors, notice: 'Logged out!')
+      redirect_to(tags_path, notice: 'Logged out!')
     end
 end
